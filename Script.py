1ee62f1271d7144f598b9742e51479613129b785
Chris Phelps <chriswphelps@yahoo.com>
2026-02-17 14:18:13 +0900

    Proof of Concept submission

--- Script.py
+++ Script.py
@@ -3,18 +3,18 @@ import requests
 import pandas as pd
 from dotenv import load_dotenv
 
-# 1. Load the key from the .env file
+#API key
 load_dotenv()
 API_KEY = os.getenv("OPENWEATHER_API_KEY")
 
 
 def get_snow_data(resort_name, lat, lon):
-    # Use metric units for Celsius
+    #temp metric set to celsius
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric"
 
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Checks for errors
+        response.raise_for_status()  #error check
         data = response.json()
 
         print(f"--- {resort_name} Status ---")
@@ -29,5 +29,5 @@ def get_snow_data(resort_name, lat, lon):
 
 
 if __name__ == "__main__":
-    # Test with Hakuba coordinates
-    get_snow_data("Hakuba", 36.6982, 137.8619)
+    #Hakuba for test
+    get_snow_data("Hakuba Valley Region", 36.6982, 137.8619)
